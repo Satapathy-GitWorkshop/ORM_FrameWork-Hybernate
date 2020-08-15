a3edcde76348c8b8d9c3da8623491f85408ac01b
Saurav <saurav.satpathy98@gmail.com>
2020-08-15 07:33:59 +0530

    Working with Dynamic table creation

---
+++ HBProj13-Dynaic_TableCreation/src/com/nt/entity/Mobile.java
@@ -0,0 +1,26 @@
+package com.nt.entity;
+
+public class Mobile {
+private Long no;
+private String brand;
+private String owner;
+public Long getNo() {
+	return no;
+}
+public void setNo(Long no) {
+	this.no = no;
+}
+public String getBrand() {
+	return brand;
+}
+public void setBrand(String brand) {
+	this.brand = brand;
+}
+public String getOwner() {
+	return owner;
+}
+public void setOwner(String owner) {
+	this.owner = owner;
+}
+
+}
---
+++ HBProj13-Dynaic_TableCreation/src/com/nt/test/Create_Table.java
@@ -0,0 +1,48 @@
+package com.nt.test;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.nt.entity.Mobile;
+import com.nt.utility.HibernateUtil;
+
+public class Create_Table {
+
+	public static void main(String[] args) {
+		Session ses=null;
+		Mobile mob=null;
+		Transaction tx=null;
+		boolean flag=true;
+		
+	//Getting Session Object
+		ses=HibernateUtil.getSession();
+	//Create enitity class Object
+        mob= new Mobile();
+        mob.setNo(2L);
+        mob.setBrand("mi2");
+        mob.setOwner("mamun2");
+        try {
+        	tx=ses.beginTransaction();	
+        		ses.save(mob);
+        		flag=true;	    	
+        }
+        catch (Exception e) {
+			// TODO: handle exception
+        	e.printStackTrace();
+        	flag=false;
+		}
+        finally {
+			if(flag) {
+				System.out.println("Object is is Saved");
+				tx.commit();
+		}
+        else {
+        	System.out.println("Object is not saved");
+        	tx.rollback();
+        }
+//closing the Connection
+			HibernateUtil.closeFactory();
+			HibernateUtil.closeSession(ses);
+        
+	}//fianlly
+}
+}
